50530ba96c4664afd1f0f592aa13c2f054fd397e
diopp <49011476+diopp@users.noreply.github.com>
2022-04-19 13:48:15 -0700

    make it better

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -27,12 +27,12 @@ export default function Example() {
     
 
 
-    <Disclosure as="nav" className="navbar sticky top-0 z-50  ">
+    <Disclosure as="nav" className="navbar sticky top-0 z-50      ">
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8  ">
+            <div className="relative flex items-center justify-between h-16  ">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden ">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="mobile inline-flex items-center justify-center p-2 rounded-md   focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
@@ -113,8 +113,8 @@ export default function Example() {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1 z-0">
+          <Disclosure.Panel className="sm:hidden ">
+            <div className="px-2 pt-2 pb-3 space-y-1 ">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
@@ -127,8 +127,7 @@ export default function Example() {
                     "block px-3 py-2 rounded-md text-base font-bold mobile  mr-5 "
                   )}
                   aria-current={item.current ? "page" : undefined}
-                  smooth={true}
-                  duration={1000}
+        
                 >
                   {item.name}
                 </Disclosure.Button>
