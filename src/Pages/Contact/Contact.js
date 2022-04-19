4e697c34e682d586371447d7b8146fde2118b7ec
diopp <49011476+diopp@users.noreply.github.com>
2022-04-19 10:40:08 -0700

    Contact form test

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -14,7 +14,7 @@ const ContactForm = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("https://portofolio0.herokuapp.com/contact", {
+    let response = await fetch("https://portofolio0.herokuapp.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
--- src/server/server.js
+++ src/server/server.js
@@ -4,10 +4,11 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
+const PORT = process.env.PORT || "3000"
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(PORT, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
