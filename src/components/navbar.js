6fab96c32f58f679b78eba742db655c1dd00a37c
diopp <49011476+diopp@users.noreply.github.com>
2022-04-19 14:13:20 -0700

    make it better

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -27,7 +27,7 @@ export default function Example() {
     
 
 
-    <Disclosure as="nav" className="navbar">
+    <Disclosure as="nav" className="navbar z-10">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8  ">
